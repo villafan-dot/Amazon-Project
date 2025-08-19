62eac93ef5451a4ba71cd9bd5e77c85bfa82875e
villafan-dot <chriswhy95@gmail.com>
2025-08-18 18:17:40 -0700

    creating a cart feature

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,3 @@
-
 let productsHTML = '';
 products.forEach((product) =>{
   productsHTML += ` 
@@ -46,7 +45,8 @@ products.forEach((product) =>{
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-product-name="${product.name}">
             Add to Cart
           </button>
         </div>
@@ -54,7 +54,31 @@ products.forEach((product) =>{
   
 });
 
-console.log(productsHTML);
+
 
 document.querySelector('.js-products-grid')
 .innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+.forEach((button) =>{
+  button.addEventListener('click',() =>{
+    const productName = button.dataset.productName;
+
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productName === item.productName) {
+        matchingItem = item;
+      }
+    });
+    if (matchingItem) {
+      matchingItem.quantity+=1
+    }
+    else{
+      cart.push({
+        productName: productName,
+        quantity: 1
+      });
+    }
+    console.log(cart);
+  });
+});
