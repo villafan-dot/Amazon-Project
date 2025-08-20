0315aedf10472f24c7313c25fd99a437bf415688
villafan-dot <chriswhy95@gmail.com>
2025-08-19 21:00:10 -0700

    Delete products from cart when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -102,7 +102,8 @@ document.querySelectorAll('.js-delete-link')
 .forEach((link) => {
   link.addEventListener('click', () => {
    const productId = link.dataset.productId; 
-   console.log(productId);
+   removeFromCart(productId);
+ 
 
   });
 });
