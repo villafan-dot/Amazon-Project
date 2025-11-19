f5ba32ebf13fd2994e1179078dcd9b015ee6a2b0
villafan-dot <chriswhy95@gmail.com>
2025-11-19 00:16:48 -0800

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,14 +4,9 @@ import {formatCurrency} from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 
-
-hello();
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-// console.log(deliveryDate);
-console.log(deliveryDate.format('dddd, MMMM D'));
 
 export function renderOrderSummary(){
 
@@ -137,6 +132,7 @@ export function renderOrderSummary(){
         `.js-cart-item-container-${productId}`
       );
       container.remove();
+      renderPaymentSummary();
         });
       });
 
@@ -146,6 +142,7 @@ export function renderOrderSummary(){
           const {productId, deliveryOptionId} = element.dataset;
           updateDeliveryOption(productId, deliveryOptionId);
           renderOrderSummary();
+          renderPaymentSummary();
         });
       });
 
