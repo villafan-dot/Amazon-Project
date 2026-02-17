20bd5e5e2a29e9bf87bbde6a9275d4b8ffd0ad1f
villafan-dot <chriswhy95@gmail.com>
2026-02-17 13:34:57 -0800

    load porducts from the backend

--- data/products.js
+++ data/products.js
@@ -93,6 +93,33 @@ const object3 = {
 object3.method();
 */
 
+
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+   // converting into a js object to use it
+    products = JSON.parse(xhr.response).map((productDetails)=> {
+      if(productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+    
+      }
+      return new Product(productDetails);
+    });
+  
+
+  console.log('load products');
+
+  fun();
+});
+
+  xhr.open('GET','https://supersimplebackend.dev/products' );
+  xhr.send();
+
+}
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -760,3 +787,4 @@ export const products = [
   return new Product(productDetails);
 });
 //map transforms whatever you put thorugh it and adds it to a new array
+*/
