9783edde0600d7a915ba3cbc26e442406038b5d6
villafan-dot <chriswhy95@gmail.com>
2026-02-18 14:37:48 -0800

    learn error handling

--- data/products.js
+++ data/products.js
@@ -113,9 +113,12 @@ export function loadProductsFetch(){
   
 
   console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.')
   });
   return promise;
 }
+
 /*
 loadProductsFetch().then(() => {
   console.log('next step');
@@ -139,11 +142,15 @@ export function loadProducts(fun) {
 
   fun();
 });
+xhr.addEventListener('error', (error)=>{
+  console.log('Unexpected error. Please try again later.')
+});
 
   xhr.open('GET','https://supersimplebackend.dev/products' );
   xhr.send();
 
 }
+
 /*
 export const products = [
   {
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -6,14 +6,26 @@ import { loadCart } from "../data/cart.js";
 // import '../data/backend-practice.js';
 
 async function loadPage() {
+  try{
+    //throw 'error1';
+
   await loadProductsFetch();
 
-  await new Promise((resolve) => {
+  const value = await new Promise((resolve,reject) => {
+   // throw 'error2'
+
+
     loadCart(() =>{
-      resolve();
+      //reject('error3');
+      resolve('value3');
     });
   });
 
+
+  } catch(error) {
+    console.log('Unexpected error. Please try again later.')
+  }
+  
   renderOrderSummary();
   renderPaymentSummary();
 }
